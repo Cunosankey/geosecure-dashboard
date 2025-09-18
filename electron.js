1a140a071a413bb87719fe15f0117d2a52302f61
Cunosankey <146829520+Cunosankey@users.noreply.github.com>
2025-09-18 18:20:56 +0200

    electron wrap
    tried wrapping it into electron, but it throws me some errors right now. Will look at this next time

---
+++ electron.js
@@ -0,0 +1,16 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true,
+    },
+  });
+  win.loadURL('http://localhost:3000'); // Next.js dev server
+}
+
+app.whenReady().then(createWindow);
